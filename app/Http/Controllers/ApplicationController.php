5387a72b0484a0c86bf0273dd9ee45f4749f9e26
Dave <johnstiger885@gmail.com>
2022-11-26 01:31:51 +0800

    [UPDATE]

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3,14 +3,52 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Http\Requests\ApplicationRequest;
+use App\Http\Services\SmsService;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Session;
 
 class ApplicationController extends Controller
 {
+    public function __construct(private SmsService $smsService)
+    {
+
+    }
+    public function index()
+    {
+        return User::selectRaw('status, count(status) as total')
+            ->where('user_role', 'driver')
+            ->groupBy('status')
+            ->get();
+    }
+
+    public function list()
+    {
+        return User::where('user_role', 'driver')
+            ->with('application')
+            ->orderBy('updated_at', 'DESC')
+            ->get();
+    }
+
+    public function confirm(User $user)
+    {
+        $this->smsService
+            ->to($user->contact_number)
+            ->message('Your application already confirmed!')
+            ->send();
+
+        return $user->update([
+            'status' => 'on_progress'
+        ]);
+    }
+
+    public function done(User $user)
+    {
+        return $user->update([
+            'status' => 'done'
+        ]);
+    }
+
     public function store(UserRequest $request)
     {
         $params = $request->validated();
