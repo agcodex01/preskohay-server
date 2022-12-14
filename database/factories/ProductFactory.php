1a331e66646bd29773d00007f9d173cd01873575
agcodex01 <agcodex01@gmail.com>
2022-12-15 05:26:03 +0800

    update

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -18,14 +18,14 @@ class ProductFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'category' => fake()->randomElement(['vegetable', 'fruits']),
-            'price_per_unit' => fake()->numberBetween(10, 100),
-            'unit' => fake()->randomElement(['kg', 'sacks', 'pcs']),
+            'name' => $this->faker->name(),
+            'category' => $this->faker->randomElement(['vegetable', 'fruits']),
+            'price_per_unit' => $this->faker->numberBetween(10, 100),
+            'unit' => $this->faker->randomElement(['kg', 'sacks', 'pcs']),
             'description' => Str::random(10),
             'image' => 'https://media.istockphoto.com/id/544652720/photo/japanese-pumpkin-kabocha-on-a-white-background.jpg?s=612x612&w=0&k=20&c=pFvYZsLfMN-B9fQU9PuhyEjt_PJp9pCxrYU_-NZiXIM=',
             'estimated_harvest_date' => now(),
-            'stocks' => fake()->numberBetween(10,100)
+            'stocks' => $this->faker->numberBetween(10,100)
         ];
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'first_name' => fake()->name(),
-            'last_name' => fake()->name(),
-            'contact_number' => fake()->phoneNumber(),
+            'first_name' => $this->faker->name(),
+            'last_name' => $this->faker->name(),
+            'contact_number' => $this->faker->phoneNumber(),
             'user_role' => 'farmer',
-            'email' => fake()->unique()->safeEmail(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
